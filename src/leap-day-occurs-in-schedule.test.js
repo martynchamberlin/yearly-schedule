db996cddd07702cf67594f6605086b8f558b95d7
Martyn Chamberlin <martynchamberlin@icloud.com>
2018-03-10 14:13:28 -0600

    So far so good; the spec is passing nicely

--- src/leap-day-occurs-in-schedule.test.js
+++ src/leap-day-occurs-in-schedule.test.js
@@ -1,13 +1,19 @@
-import { createLeapYear } from './test-helpers';
+import { createLeapYear, createNonLeapYear } from './test-helpers';
 import { leapDayOccursInSchedule } from './leap-day-occurs-in-schedule';
 
 describe('leapDayOccursInSchedule', () => {
   const sampleRanges = ['01-01', '01-02', '02-28', '03-01', '12-30', '12-31'];
   const sampleRangesWithLeapYear = [...sampleRanges, '02-29'];
 
-  test('Should occur in schedule with start date of January 1 and any current day', () => {
-    sampleRanges.forEach((range) => {
+  test('Should occur with leap year in schedule with start date of January 1 and any current day', () => {
+    sampleRangesWithLeapYear.forEach((range) => {
       expect(leapDayOccursInSchedule('01-01', createLeapYear(range))).toEqual(true);
+    });
+  });
+
+  test('Should not occur in non-leap year in schedule with start date of January 1 and any current day', () => {
+    sampleRanges.forEach((range) => {
+      expect(leapDayOccursInSchedule('01-01', createNonLeapYear(range))).toEqual(false);
     });
   });
 });
